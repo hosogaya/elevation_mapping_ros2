743c0ebc1c71bc8db16b019145f8d5fa6749c3a5
hosogaya <hosogaya.hirotaka@gmail.com>
2023-07-27 03:27:36 +0900

    read parameters from yaml file. remap topics

--- launch/elevation_mapping.launch.py
+++ launch/elevation_mapping.launch.py
@@ -5,6 +5,11 @@ import os
 import yaml
 
 
+def read_yaml(file_path):
+    with open(file_path, 'r') as f:
+        data = yaml.safe_load(f)
+        params = data['/**']['ros__parameters']
+    return params
 
 def generate_launch_description():
     elevation_map_param_file = os.path.join(
@@ -12,16 +17,22 @@ def generate_launch_description():
         'config',
         'elevation_map.param.yaml')
     
-    with open(elevation_map_param_file, 'r') as f:
-        data = yaml.safe_load(f)
-        params = data['/**']['ros__prameters']
+    topic_name_file = os.path.join(
+        get_package_share_directory("elevation_mapping_ros2"), 
+        'config', 
+        'topic_name.yaml')
     
-    
+    params = read_yaml(elevation_map_param_file)
+    topic_name = read_yaml(topic_name_file)
+        
     elevation_mapping_node = Node(
         package="elevation_mapping_ros2",
         name="elevation_mapping_ros2_node",
         executable="elevation_mapping_ros2_node",
         parameters=[params], 
+        remappings=[("input/point_cloud", topic_name["input"]["point_cloud"]), 
+                    ("input/pose", topic_name["input"]["pose_covariance"]), 
+                    ("output/raw_map", topic_name["output"]["raw_map"])], 
     )
     return LaunchDescription([
         elevation_mapping_node,
